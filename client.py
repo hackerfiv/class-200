6dcbc97dccafd6a41f62c74e118d91d3f20d21c7
hackerfiv <82275827+hackerfiv@users.noreply.github.com>
2023-03-28 19:46:31 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,53 @@
+import socket
+from  threading import Thread
+
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+nickname=input("choose your nickname ")
+ip_address="127.0.0.1"
+port=8000
+client.connect((ip_address,port))
+print("connected with this server ")
+
+
+
+def receive():
+    while True :
+        try:
+            message=client.recv(2048).decode("utf-8")
+            if message=="NICKNAME":
+                client.send(nickname.encode("utf-8"))
+            else:
+                print(message)
+        except:
+            print("error")
+            client.close()
+            break
+
+
+def write():
+    while True:
+        message="{}:{}".format(nickname,input(""))
+        client.send(message.encode("utf-8"))
+
+receive_thread=Thread(target=receive)
+receive_thread.start()
+write_thread=Thread(target=write)
+write_thread.start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
